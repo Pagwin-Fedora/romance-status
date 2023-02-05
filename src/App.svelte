60a30bebcc926e17bd913710ee3c82311b75e602
Pagwin <git@pagwin.xyz>
2023-02-04 23:19:43 -0500

    omg I'm an idiot this should be in VCS

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Table from './lib/Table.svelte';
+    import { onMount } from 'svelte';
+    let data = [];
+    function sleep(n){
+	return new Promise((res,rej)=>setTimeout(res,n))
+    }
+    onMount(async ()=>{
+	while(true){
+	    try{
+		data = await fetch(window.origin + "/status.json").then(res=>res.json());
+	    }
+	    catch(e){}
+	    console.log(data);
+	    await sleep(500);
+	}
+    });
+</script>
+
+<main>
+    <div id="center">
+    <Table content={data}></Table>
+    </div>
+</main>
+<style>
+div{
+    display: block;
+    margin: auto;
+    width:50%;
+}
+
+</style>
---
+++ src/lib/Table.svelte
@@ -0,0 +1,44 @@
+<script>
+import "@picocss/pico/css/pico.min.css";
+export let content;
+const table = {
+    Complete: "color:green;",
+    Failure: "color:red;",
+    Pending: "color:#bbb;",
+    Ongoing: "color:#36f;"
+}
+</script>
+<table role="grid" style="display: inline-table">
+    <thead>
+	<tr>
+	    <th>Job Name</th>
+	    <th>Job Status</th>
+	</tr>
+    </thead>
+    <tbody>
+	{#each content as item}
+	    <tr>
+		<td>{item[0]}</td>
+		<td style={table[item[1]]}>{item[1]}</td>
+	    </tr>
+	{/each}
+    </tbody>
+</table>
+<style>
+    table {
+	border: 5px solid white;
+    }
+    th{
+	border: 2px solid white;
+	text-align: center;
+    }
+    tr {
+	border: 5px solid white;
+    }
+    td{
+	border: 2px solid white;
+	margin: auto;
+	border-collapse: collapse;
+	text-align: center;
+    }
+</style>
